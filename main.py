f2c606abb16ccde43ac23ce26350c5e9525e053c
russele7 <russele7oge@gmail.com>
2025-04-11 21:52:07 +0300

    tutorial 2

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
+from pydantic import BaseModel
+
+
+class NewBook(BaseModel):
+    title: str
+    author: str
+
 
 app = FastAPI()
 
+books = [
+    {
+        "id": 1,
+        "title": "Асинхронность в Python",
+        "author": "Matthew",
+    },
+    {
+        "id": 2,
+        "title": "Backend разработка в Python",
+        "author": "Артем",
+    }
+]
 
-@app.get('/', summary='Main root', tags=['osnova'])
-def root():
-    return 'Hello World!!!'
+
+@app.get('/books/',
+         tags=['books'],
+         summary='list all books')
+def read_books():
+    return books
+
+
+@app.get('/books/{book_id}',
+         tags=['books'],
+         summary='get book by id')
+def get_book(book_id: int):
+    for book in books:
+        if book['id'] == book_id:
+            return book
+    raise HTTPException(404, detail='book didnt find')
+
+
+@app.post('/books/create', tags=['books'])
+def create_book(new_book: NewBook):
+    books.append(
+        {
+            "id": len(books) + 1,
+            "title": new_book.title,
+            "author": new_book.author
+        }
+    )
+    return {'success': True, 'message': f'book {books[-1]} created'}
 
 
 if __name__ == '__main__':
